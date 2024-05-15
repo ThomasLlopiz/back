3f208cd37d4fd82f51c4c98d385da8799b535c2b
ThomasLlopiz <toomillopiz@gmail.com>
2024-05-15 13:22:37 -0300

    push change

---
+++ server.js
@@ -0,0 +1,39 @@
+const express = require('express')
+const mysql = require('mysql')
+const cors = require('cors')
+
+
+const app = express()
+app.use(cors())
+// servername : "localhost:3306",
+// username : "grupodev_speratta",
+// password : "Santiago@2024",
+// database : "grupodev_golden",
+const db = mysql.createConnection({
+    host: "localhost",
+    user: 'root',
+    password: '',
+    database: 'cartones'
+})
+
+app.get('/', (req, res) => {
+    return res.json("server on")
+})
+
+app.get('/agosto', (req, res) => {
+    const sql = "SELECT  * FROM agosto"
+    db.query(sql, (err, data) => {
+        if (err) return res.json(err)
+        return res.json(data)
+    })
+})
+app.get('/participantes', (req, res) => {
+    const sql = "SELECT  * FROM participantes"
+    db.query(sql, (err, data) => {
+        if (err) return res.json(err)
+        return res.json(data)
+    })
+})
+app.listen(5050, () => {
+    console.log("http://localhost:5050/participantes")
+})
